749fc287d8037f78efefae2044234935955f3d24
이유진 <elli.stdev@gmail.com>
2024-07-01 13:41:42 +0900

    01.variables

---
+++ src/variable/Var8.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127; // -128 ~ 127
+        short s = 32767; // -32768 ~ 32767
+        int i = 2147483647; // 약 21억
+
+        // 가장 긴 정수, long은 끝에 대문자 L
+        long l = 9223370236854775807L;
+
+        // 실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}

b203e74e87d653778ef90e35171514328b319877
이유진 <elli.stdev@gmail.com>
2024-07-20 21:49:31 +0900

    feat: Polymorphism2

---
+++ src/basic/poly/ex2/AnimalPolyMain3.java
@@ -0,0 +1,13 @@
+package basic.poly.ex2;
+
+public class AnimalPolyMain3 {
+    public static void main(String[] args) {
+        Animal[] animalArr = {new Dog(), new Cat(), new Cow()};
+
+        for (Animal animal : animalArr) {
+            System.out.println("동물 소리 테스트 시작");
+            animal.sound();
+            System.out.println("동물 소리 테스트 종료");
+        }
+    }
+}
---
+++ src/basic/poly/ex4/Cow.java
@@ -0,0 +1,12 @@
+package basic.poly.ex4;
+
+public class Cow extends AbstractAnimal {
+    @Override
+    public void sound() {
+        System.out.println("음메");
+    }
+    @Override
+    public void move() {
+        System.out.println("송아지 이동");
+    }
+}

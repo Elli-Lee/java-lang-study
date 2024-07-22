a106ad2891f09fcab8c9612bf8cd1b60d2a29ae2
이유진 <elli.stdev@gmail.com>
2024-07-22 20:24:34 +0900

    feat: Object Class

---
+++ src/intermediate1/lang/LangMain.java
@@ -0,0 +1,8 @@
+package intermediate1.lang;
+
+import java.lang.System;
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ src/intermediate1/lang/object/tostring/Car.java
@@ -0,0 +1,8 @@
+package intermediate1.lang.object.tostring;
+
+public class Car {
+    private String carName;
+    public Car (String carName) {
+        this.carName = carName;
+    }
+}
